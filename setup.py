f750926cc590f742977bcbfe3463ed6688d3621a
dexterchan <dexterchan@gmail.com>
2023-03-02 22:09:10 +0800

    add pytest.ini and setup.py modification

--- setup.py
+++ setup.py
@@ -1,17 +1,44 @@
 # !/usr/bin/env python
-
+import setuptools
 from distutils.core import setup
+from setuptools import find_packages
+import os
+"""The setup script."""
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements_file_lst = ["requirements.txt"]
+install_req = []
+directory = os.getcwd()
+
+for file in requirements_file_lst:
+    with open(os.path.join(directory, file), "r", encoding="utf-8") as fr:
+        req_texts = [n.strip() for n in fr.readlines()]
+        req_texts = [t for t in req_texts if len(t) > 0]
+        install_req.extend(req_texts)
+test_requirements = ['pytest>=3', ]
+
+
 setup(
     name='cookiecutter-pypackage',
     packages=[],
     version='0.1.0',
     description='Cookiecutter template for a Python package',
-    author='Audrey Roy Greenfeld',
+    author='Dexter Chan',
     license='BSD',
-    author_email='aroy@alum.mit.edu',
+    author_email='dex@abc.com',
     url='https://github.com/audreyr/cookiecutter-pypackage',
     keywords=['cookiecutter', 'template', 'package', ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    test_suite='tests',
+    tests_require=test_requirements,
+    packages=setuptools.find_packages(where="src"),
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
